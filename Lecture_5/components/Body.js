12b4422b41a48327da2ab128d6f2391124a016fc
Himanshu <himanshu.singh_iot18@gla.ac.in>
2023-01-16 23:20:13 +0530

    adding shimmer effect

--- Lecture_5/components/Body.js
+++ Lecture_5/components/Body.js
@@ -1,45 +1,47 @@
 import { restrauList } from "../constants";
 import ReasturantCard from "./ReasturantCard";
-import { useState,useEffect } from "react";
-const filterData = (searchTxt,restaurant) => {
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
-return restaurant.filter((restra)=>restra.data.name.includes(searchTxt))
-    
-}
+
+const filterData = (searchTxt, restaurant) => {
+  return restaurant.filter((restra) => restra.data.name.includes(searchTxt));
+};
 
 const Body = () => {
   // let searchTxt = "Chicken"
-  const [restaurant,setRestaurant] = useState(restrauList);
-  const [searchTxt,setSearchTxt] = useState("");
+  const [allrestaurant, setAllRestaurant] = useState([]);
+  const [filteredrestaurant, setFilteredRestaurant] = useState([]);
+  const [searchTxt, setSearchTxt] = useState("");
 
-  useEffect(()=>{
-    // if(searchTxt==''){
-      console.log("himan")
-      setRestaurant(restrauList)
-    // }
-  },[searchTxt,restrauList]);
-   
-  useEffect(()=>{
+  useEffect(() => {
+    if (searchTxt === "") {
+      setFilteredRestaurant(allrestaurant);
+    }
+  }, [searchTxt]);
+
+  useEffect(() => {
     getRestaurant();
-    // getUser();
-  },[])
+  }, []);
 
-
-  async function  getRestaurant()  {
-  const data =  await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8466937&lng=80.94616599999999&page_type=DESKTOP_WEB_LISTING");
-  const json = await data.json();
-  console.log(json);
-  } 
+  async function getRestaurant() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8466937&lng=80.94616599999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setAllRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+    setFilteredRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+  }
 
   // async function getUser() {
   //   try {
-  
+
   //     const response = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8466937&lng=80.94616599999999&page_type=DESKTOP_WEB_LISTING',{mode:'cors'});
-  
+
   //     if (!response.ok) {
   //       throw new Error(`Error! status: ${response.status}`);
   //     }
-  
+
   //     const result = await response.json();
   //     console.log(result);
   //     return result;
@@ -47,40 +49,36 @@ const Body = () => {
   //     console.log(err);
   //   }
   // }
-  
-    return (
-      <>
-      
+
+  return  allrestaurant.length===0?(<Shimmer/>):(
+    <>
       <div className="search-container">
-      <input
-      type="text"
-      className="search-input"
-      placeholder="Search"
-      value={searchTxt}
-      onChange={(e)=>setSearchTxt(e.target.value)}
-      />
-      <button 
-      className="search-btn"
-      onClick={()=>{const data = filterData(searchTxt,restaurant);
-      setRestaurant(data)
-      }}
-      > 
-      Search
-      </button>
-
-
-
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchTxt}
+          onChange={(e) => setSearchTxt(e.target.value)}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = filterData(searchTxt, allrestaurant);
+            setFilteredRestaurant(data);
+          }}
+        >
+          Search
+        </button>
       </div>
       <div className="restaurant-list">
-      
-       {restaurant.map((restaurant)=>{
-        return <ReasturantCard {...restaurant.data} key={restaurant.data.id}/>
-        
-       })}
-      
+        {filteredrestaurant.map((restaurant) => {
+          return (
+            <ReasturantCard {...restaurant.data} key={restaurant.data.id} />
+          );
+        })}
       </div>
-      </>
-    )
-  }
+    </>
+  );
+};
 
 export default Body;
